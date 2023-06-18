3e7aec75dff21475064fd8adb64fcda679d958ac
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-18 16:39:36 +0300

    project blueprint

---
+++ assets/blueprint/blueprint-url.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project blueprint</title>
+</head>
+<body>
+    <a href="http://https://preview.colorlib.com/#thevenue" target="_blank" rel="noopener noreferrer"> PHP PROJECT BLUEPRINT</a>
+</body>
+</html>
