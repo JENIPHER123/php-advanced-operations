73e97a101d053ab75a273b9cda4ae36c7536fb5b
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-19 14:22:57 +0300

    home screen finish

--- index.php
+++ index.php
@@ -9,51 +9,77 @@
     <!-- css links -->
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    
     
 </head>
 <body>
 
     <!-- main container -->
-    <div class="cont1 "> 
-        <div class="navigation ">
+    <div class="cont1 overflow-hidden"> 
+      <div class="cont1-inner">
+      <div class="navigation ">
         <nav class="navbar navbar-expand-lg navbar-dark  ">
           <div class="container-fluid p-4">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand px-5 " href="#"><h1>PHP-OPERATIONS</h1> </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  <a class="nav-link active" aria-current="page" href="#">HOME</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
+                  <a class="nav-link " aria-current="page" href="#">CRUD OPERATIONS</a>
                 </li>
+                
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                API INTEGRATION
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                 
+                 
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                AI $ DATA MINING
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                 
+                 
+                </ul>
+              </li>
+               
+              
+                <li class="nav-item mx-4" style="border: 1px solid white;">
+                  <a class="nav-link active " aria-current="page" href="#"> LEARN MORE TRICKS AND  FACTS</a>
                 </li>
               </ul>
-              <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
+             
+             
             </div>
+
           </div>
         </nav> 
         </div>
+         <div class="cont2 d-flex justify-content-center align-items-center" style="height: 60vh;">
+          <div class="cont2-inner">
+          <h1 class="text-center " style="color: rgb(235, 180, 109);font-family:'Tangerine', serif; font-weight:bold;">Welcome to the future of PHP... <br>Coding with Jenny!!</h1>
+          <h1 class="text-center text-light">DESCOVER MORE WITH PHP</h1>
+          <P class="text-center text-light" style="font-family: 'Tangerine', serif;">Your not allowed to let yourself give up!! You got this!!</P>
+
+          </div>
+         
+         </div>
+      </div>
+        
     </div>
     
 
