8d60a27661f7b48b37895a65cd09d26ae377899a
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-20 16:04:57 +0300

    pages header

---
+++ crud/crud-landing.php
@@ -0,0 +1,1 @@
+<?php include_once '../partials/subscribe.php'?>
--- partials/navbar.php
+++ partials/navbar.php
@@ -18,10 +18,10 @@
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   API INTEGRATION
                   </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="chatbot/call-landing.php">CALL API</a></li>
-                    <li><a class="dropdown-item" href="api-intergration/sms-landing.php">SMS API</a></li>
-                    <li><a class="dropdown-item" href="AI-ask/api-landing.php">API OTHER</a></li>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown"style="  background: rgba(0, 0, 0, 0.6);">
+                    <li><a class="dropdown-item text-light" href="chatbot/call-landing.php">CALL API</a></li>
+                    <li><a class="dropdown-item text-light" href="api-intergration/sms-landing.php">SMS API</a></li>
+                    <li><a class="dropdown-item text-light" href="AI-ask/api-landing.php">API OTHER</a></li>
                   
                   </ul>
                 </li>
@@ -29,17 +29,17 @@
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   AI $ DATA MINING
                   </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="chatbot/chatbot-landing.php">CHATBOT</a></li>
-                    <li><a class="dropdown-item" href="AI-ask/ai-ask-landing.php">AI ASK</a></li>
-                    <li><a class="dropdown-item" href="AI-ask/datamin-landing.php">DATA MINING</a></li>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown"style=" background: rgba(0, 0, 0, 0.6);">
+                    <li><a class="dropdown-item text-light" href="chatbot/chatbot-landing.php">CHATBOT</a></li>
+                    <li><a class="dropdown-item text-light" href="AI-ask/ai-ask-landing.php">AI ASK</a></li>
+                    <li><a class="dropdown-item text-light" href="AI-ask/datamin-landing.php">DATA MINING</a></li>
                   
                   </ul>
                 </li>
                 
                 
                   <li class="nav-item mx-4" style="border: 1px solid white;">
-                    <a class="nav-link active " aria-current="page" href="#"> LEARN MORE TRICKS AND  FACTS</a>
+                    <a class="nav-link active "target="blank" aria-current="page" href="https://github.com/JENIPHER123"> LEARN MORE TRICKS AND  FACTS</a>
                   </li>
                 </ul>
               
