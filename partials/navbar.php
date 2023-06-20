f070310dcbab80b04ea81ef6134cc1c637627345
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-20 17:55:38 +0300

    adding links

--- partials/navbar.php
+++ partials/navbar.php
@@ -11,33 +11,21 @@
                     <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link " aria-current="page" href="crud/crud-landing.php">CRUD OPERATIONS</a>
+                    <a class="nav-link "target="blank" aria-current="page" href="index.php">LINKED IN</a>
                   </li>
                   
-                  <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  API INTEGRATION
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown"style="  background: rgba(0, 0, 0, 0.6);">
-                    <li><a class="dropdown-item text-light" href="chatbot/call-landing.php">CALL API</a></li>
-                    <li><a class="dropdown-item text-light" href="api-intergration/sms-landing.php">SMS API</a></li>
-                    <li><a class="dropdown-item text-light" href="AI-ask/api-landing.php">API OTHER</a></li>
-                  
-                  </ul>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  AI $ DATA MINING
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown"style=" background: rgba(0, 0, 0, 0.6);">
-                    <li><a class="dropdown-item text-light" href="chatbot/chatbot-landing.php">CHATBOT</a></li>
-                    <li><a class="dropdown-item text-light" href="AI-ask/ai-ask-landing.php">AI ASK</a></li>
-                    <li><a class="dropdown-item text-light" href="AI-ask/datamin-landing.php">DATA MINING</a></li>
-                  
-                  </ul>
-                </li>
-                
-                
+                  <li class="nav-item">
+                    <a class="nav-link "target="blank" aria-current="page" href="https://jenipher-kuki.000webhostapp.com/">WEBSITE</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link "target="blank" aria-current="page" href="https://www.youtube.com/@_BYTESCAPE">YOUTUBE</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="index.php">CONTACT</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="index.php">LOGIN</a>
+                  </li>
                   <li class="nav-item mx-4" style="border: 1px solid white;">
                     <a class="nav-link active "target="blank" aria-current="page" href="https://github.com/JENIPHER123"> LEARN MORE TRICKS AND  FACTS</a>
                   </li>
