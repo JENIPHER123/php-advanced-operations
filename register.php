7efd0bdc608d6b1023d44bf30f1479913373a177
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-26 17:29:35 +0300

    login register UI

--- register.php
+++ register.php
@@ -24,12 +24,26 @@ include_once "partials/navbar.php"?>
           <div class="card">
             <div class="card-body py-5 px-md-5">
               <form>
-                <!-- 2 column grid layout with text inputs for the first and last names -->
-                <div class="row">
+              <div class="row">
                   <div class="col-md-12 mb-5">
-                    <div class="h1 fw-bold text-center">LOGIN</div>
+                    <div class="h1 fw-bold text-center">REGISTER</div>
                   </div>
                  
+                </div>
+                <!-- 2 column grid layout with text inputs for the first and last names -->
+                <div class="row">
+                  <div class="col-md-6 mb-4">
+                    <div class="form-outline">
+                      <input type="text" id="form3Example1" class="form-control" />
+                      <label class="form-label" for="form3Example1">First name</label>
+                    </div>
+                  </div>
+                  <div class="col-md-6 mb-4">
+                    <div class="form-outline">
+                      <input type="text" id="form3Example2" class="form-control" />
+                      <label class="form-label" for="form3Example2">Last name</label>
+                    </div>
+                  </div>
                 </div>
 
                 <!-- Email input -->
@@ -54,12 +68,12 @@ include_once "partials/navbar.php"?>
 
                 <!-- Submit button -->
                 <button type="submit" class="btn btn-primary btn-block mb-4 form-control">
-                  Sign in
+                  Sign up
                 </button>
 
                 <!-- Register buttons -->
                 <div class="text-center">
-                  <p>Don't have an account? <a href="register.php">register here!!</a></p>
+                  <p>Already have an account? <a href="register.php">login here!!</a></p>
                   <button type="button" class="btn btn-link btn-floating mx-1">
                     <i class="fab fa-facebook-f"></i>
                   </button>
