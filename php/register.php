41735c2749587c4fce61dde7429e375ec79a8a3d
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-26 18:59:56 +0300

    register function

--- php/register.php
+++ php/register.php
@@ -1,4 +1,25 @@
 <?php
 
+/* db connection */
+include_once 'conection.php';
 
+/* variable declaration */
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$pass = $_POST['password'];
+
+/* logging in */
+$login = "INSERT INTO users (first_name, last_name, email, password) VALUES ('$first_name','$last_name','$email','$pass')";
+
+$query = mysqli_query($conn, $login);
+
+
+if($query){
+    echo "reg successful";
+  
+}
+else{
+    echo "error";
+}
 ?>
