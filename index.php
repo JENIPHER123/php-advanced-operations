23aa42af1b4673c53564304627548481bf49b480
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-26 15:33:32 +0300

    UI redesign

--- index.php
+++ index.php
@@ -1,57 +1,7 @@
 
-<?php include_once "partials/header.php"?>
-    <!-- main container -->
-    <div class="cont1 overflow-hidden"> 
-      <div class="cont1-inner">
-      <?php include_once "partials/navbar.php"?> 
-         <div class="cont2 d-flex justify-content-center align-items-center" style="height: 60vh;">
-          <div class="cont2-inner">
-          <h1 class="text-center " style="color: rgb(235, 180, 109);font-family:'Tangerine', serif; font-weight:bold;">Welcome to the future of PHP... <br>Coding with Jenny!!</h1>
-          <h1 class="text-center text-light">DESCOVER MORE WITH PHP</h1>
-          <P class="text-center text-light" style="font-family:  serif;">Your not allowed to let yourself give up!! You got this!!</P>
+<?php 
+include_once "partials/header.php";
+include_once "partials/navbar.php"?>
+  
 
-          </div>
-         
-         </div>
-      </div>
-        
-    </div>
-    <!-- main container! -->
-
-    <!-- About area -->
-    
-
-    <div class="row" style="width: 100%;">
-
-      <div class="col-sm-4 d-flex justify-content-center align-items-center">
-          <div class="cont3 p-5">
-            <div class="title text-center pt-2 h1" style="font-family:'Tangerine', serif; font-weight:bold;">About The Project</div>
-            <div class="sub-title text-center pb-3 text-secondary" >This is a PHP project</div>
-            <p class="text-center">This project is aimed at exploiting and manipulating PHP code to perform fundamental functions. With these, you can be able to create any system required by a respective client</p>
-          </div>
-      </div>
-
-      
-      <div class="col-sm-4 d-flex justify-content-center align-items-center">
-          <div class="cont3 p-5">
-            <div class="title text-center pt-2 h1" style="font-family:'Tangerine', serif; font-weight:bold;">About Myself</div>
-            <div class="sub-title text-center pb-3 text-secondary" >Full-stack software developer</div>
-            <p class="text-center">I'm Jenipher a full-stack developer. I also develop apps and am an expert in AI development using python(tensor flow) and matlab. I'm also a designer and teacher. <br><a href="https://jenipher-kuki.000webhostapp.com/" target="_blank" rel="noopener noreferrer">See more</a></p>
-          </div>
-      </div>
-
-
-     
-      <div class="col-sm-4 d-flex justify-content-center align-items-center">
-          <div class="cont3 p-5">
-            <div class="title text-center pt-2 h1" style="font-family:'Tangerine', serif; font-weight:bold;">About My Company</div>
-            <div class="sub-title text-center pb-3 text-secondary" >BYTESAOPE TECHNOLOGIES AFRICA</div>
-            <p class="text-center">I (Jenipher A. Onyango) am the founder and CEO of BYTESCAPE TECHNOLOGIES AFRICA.A company aimed towards solving Africa's problems using technology. <br><a href="#" target="_blank" rel="noopener noreferrer">See more</a></p>
-          </div>
-      </div>
-
-
-    </div>
-    <!-- About area! -->
-
-    <?php include_once "partials/footer.php"?>
+<?php include_once "partials/footer.php"?>
