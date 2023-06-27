eb6d4858deca993a0ec120e96747dc0383a2f24e
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-27 23:56:27 +0300

    main content page area

--- blueprint/encryption/encrypt-landing.php
+++ blueprint/encryption/encrypt-landing.php
@@ -5,7 +5,9 @@ include_once '../partials/page_nav.php';
 
 
         <!-- page content -->
-      
+        <div class="right_col" role="main">
+          
+          </div>
         <!-- /page content -->
 
 
