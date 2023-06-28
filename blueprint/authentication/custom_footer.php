e93e0e1a05730872b2f7d0761fa488169168c084
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-28 21:48:52 +0300

    email auth firebase ui init

---
+++ blueprint/authentication/custom_footer.php
@@ -0,0 +1,27 @@
+   <!-- footer content -->
+   <footer>
+          <div class="pull-right">
+            Project dome by <a href="https://jenipher-kuki.000webhostapp.com/">Jenipher A. Onyango</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+   <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    <!-- jQuery Smart Wizard -->
+    <script src="../vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js"></script>
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+
+	
+  </body>
+</html>
--- blueprint/authentication/email-auth-firebase.php
+++ blueprint/authentication/email-auth-firebase.php
@@ -313,30 +313,4 @@
         </div>
         <!-- /page content -->
 
-       <!-- footer content -->
-       <footer>
-          <div class="pull-right">
-            Project dome by <a href="https://jenipher-kuki.000webhostapp.com/">Jenipher A. Onyango</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
-      </div>
-    </div>
-
-    <!-- jQuery -->
-    <script src="../vendors/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap -->
-   <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- FastClick -->
-    <script src="../vendors/fastclick/lib/fastclick.js"></script>
-    <!-- NProgress -->
-    <script src="../vendors/nprogress/nprogress.js"></script>
-    <!-- jQuery Smart Wizard -->
-    <script src="../vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js"></script>
-    <!-- Custom Theme Scripts -->
-    <script src="../build/js/custom.min.js"></script>
-
-	
-  </body>
-</html>
+    <?php include_once 'custom_footer.php'?>
