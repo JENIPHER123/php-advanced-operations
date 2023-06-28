236b2e343e007cd2448ea401263cbf09dcf8e954
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-28 17:00:41 +0300

    dash content init

--- blueprint/main-landing/main-landing.php
+++ blueprint/main-landing/main-landing.php
@@ -3,12 +3,112 @@ include_once '../partials/header2.php';
 include_once '../partials/page_nav.php';
 ?>
 
+                <!-- page content -->
+                <div class="right_col" role="main">
+                        <div class="">
+                        <div class="page-title">
+                        <div class="title_left">
+                                <h3>Welcome to Codding with Jenny!<!-- <small> design</small> --></h3>
+                        </div>
 
-        <!-- page content -->
-        <div class="right_col" role="main">
-          
-        </div>
-        <!-- /page content -->
+                        <div class="title_right">
+                                <div class="col-md-5 col-sm-5   form-group pull-right top_search">
+                                <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search for...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-secondary" type="button">Go!</button>
+                                </span>
+                                </div>
+                                </div>
+                        </div>
+                        </div>
+                        
+                        <div class="clearfix"></div>
+
+                        <div class="row">
+                        <div class="col-md-12">
+                                <div class="x_panel">
+                                <div class="x_title">
+                                <h2>New Partner Contracts Consultancy</h2>
+                                <ul class="nav navbar-right panel_toolbox">
+                                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                </li>
+                                <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#">Settings 1</a>
+                                        <a class="dropdown-item" href="#">Settings 2</a>
+                                        </div>
+                                </li>
+                                
+                                </ul>
+                                <div class="clearfix"></div>
+                                </div>
+
+                                <div class="x_content">
+
+                                <div class="col-md-9 col-sm-9  ">
+
+                                <ul class="stats-overview">
+                                        <li>
+                                        <span class="name"> System Projects Completed </span>
+                                        <span class="value text-success"> 2 </span>
+                                        </li>
+                                        <li>
+                                        <span class="name">NO. of Apps Completed </span>
+                                        <span class="value text-success"> 0 </span>
+                                        </li>
+                                        <li class="hidden-phone">
+                                        <span class="name">Pending Projects</span>
+                                        <span class="value text-success"> 13 </span>
+                                        </li>
+                                      
+                                </ul>
+                                <br />
+
+                                <div id="mainb" style="height:350px;"></div>
+
+              
 
 
+                                </div>
+
+                                <!-- start project-detail sidebar -->
+                                <div class="col-md-3 col-sm-3  ">
+
+                                <section class="panel">
+
+                                        <div class="x_title">
+                                        <h2>Project Description</h2>
+                                        <div class="clearfix"></div>
+                                        </div>
+                                        <div class="panel-body">
+                                        <h3 class="green"><i class="fa fa-paint-brush"></i> PHP Operations</h3>
+
+                                        <p>These are all the fundamental PHP operations that are necessary for any and/or all systems. Upon further review you'll realize that all systems contain many if not most of these functions.</p>
+                                        <br />
+
+                                        <div class="project_detail">
+
+                                        <p class="title">Visit My Website</p>
+                                        <p><a href="https://jenipher-kuki.000webhostapp.com/" target="_blank" rel="noopener noreferrer">@_JenipherAkinyiOnyango</a></p>
+                                        <p class="title">Visit My Github</p>
+                                        <p><a href="https://github.com/JENIPHER123" target="_blank" rel="noopener noreferrer">@_Jenipher123</a></p>
+                                        </div>
+
+                                        </div>
+
+                                </section>
+
+                                </div>
+                                <!-- end project-detail sidebar -->
+
+                                </div>
+                                </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        <!-- /page content -->
+                        
 <?php include_once '../partials/page_footer.php'?>
